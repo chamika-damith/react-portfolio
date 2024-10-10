684869dc751874ef77ee2e12ae98ae3fe240190a
chamika-damith <chamikadamith9@gmail.com>
2024-10-10 15:40:39 +0530

    update technology icons title

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,22 +8,22 @@ function Technologies() {
         <div className="pb-24">
             <h2 className="my-20 text-center text-4xl">Technologies</h2>
             <div className="flex flex-wrap items-center justify-center gap-10">
-                <div>
+                <div title="React">
                     <RiReactjsLine className="text-7xl text-cyan-400" />
                 </div>
-                <div>
+                <div title="Nextjs">
                     <TbBrandNextjs className="text-7xl" />
                 </div>
-                <div>
+                <div title="Mongodb">
                     <SiMongodb className="text-7xl text-green-800" />
                 </div>
-                <div>
+                <div title="Mysql">
                     <SiMysql  className="text-7xl text-blue-500" />
                 </div>
-                <div>
+                <div title="Nodejs">
                     <FaNode className="text-7xl text-green-500" />
                 </div>
-                <div>
+                <div title="Java">
                     <FaJava  className="text-7xl text-orange-400" />
                 </div>
             </div>
