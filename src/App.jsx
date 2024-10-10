3eb3d41fdbd4f790bcfd01208007a8bd4aabbc4b
chamika-damith <chamikadamith9@gmail.com>
2024-10-10 14:01:52 +0530

    create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -12,8 +13,9 @@ export default function App() {
         bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div></div>
       </div>
 
-      <div className="container mx-auto px-8">
+      <div className="container mx-auto px-32">
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import profilePic from '../assets/profilePicnew.jpg';
+import { HERO_CONTENT } from '../assets/constants';
+
+function Hero() {
+    return (
+        <div className="pb-4 lg:mb-36">
+            <div className="flex flex-wrap lg:flex-row-reverse">
+            <div className="w-full lg:w-1/2">
+                    <div className='flex justify-center lg:justify-end lg:p-8 mr-5'>
+                        <img src={profilePic} alt="chamika damith" className="border border-stone-900 rounded-3xl  transition-all duration-300 
+                        cursor-pointer filter grayscale hover:grayscale-0
+                        " width={400} height={550} />
+                    </div>
+                </div>
+
+                <div className='w-full lg:w-1/2'>
+                    <div className='flex flex-col items-center lg:items-start mt-10 lg:mt-0'>
+                        <h2 className='pb-3 text-4xl tracking-tighter lg:text-8xl'>
+                            Chamika Damith
+                        </h2>
+                        <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight'>
+                            Full Stack Developer
+                        </span>
+                        <p className='my-2 max-w-2xl py-6 text-xl leading-relaxed tracking-tight'>
+                            {HERO_CONTENT}
+                        </p>
+                        <a href='/resume.pdf' target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>
+                            Download Resume
+                        </a>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
